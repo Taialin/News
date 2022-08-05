78ce35f632b49a6e28fd22e293c773d8c816f64d
Bylich <65658343+Taialin@users.noreply.github.com>
2022-08-05 12:03:06 +0300

    Delete src directory

--- src/main/java/com/example/news/repository/SubscriptionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.news.repository;
-
-import com.example.news.dob.Subscriptions;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubscriptionsRepository extends CrudRepository<Subscriptions, Long> {
-
-}
